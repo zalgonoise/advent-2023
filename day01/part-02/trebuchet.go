1af679def2023bb50ae96f609d4cfbf31a56aa54
zalgonoise <mariozalgo@gmail.com>
2023-12-01 15:32:03 +0100

    advent/day01/part-02: remove unused references

--- day01/part-02/trebuchet.go
+++ day01/part-02/trebuchet.go
@@ -4,48 +4,6 @@ import (
 	"strings"
 )
 
-var numbers = []string{
-	"one",
-	"two",
-	"three",
-	"four",
-	"five",
-	"six",
-	"seven",
-	"eight",
-	"nine",
-	"1",
-	"2",
-	"3",
-	"4",
-	"5",
-	"6",
-	"7",
-	"8",
-	"9",
-}
-
-var numStrings = map[string]int{
-	"one":   1,
-	"two":   2,
-	"three": 3,
-	"four":  4,
-	"five":  5,
-	"six":   6,
-	"seven": 7,
-	"eight": 8,
-	"nine":  9,
-	"1":     1,
-	"2":     2,
-	"3":     3,
-	"4":     4,
-	"5":     5,
-	"6":     6,
-	"7":     7,
-	"8":     8,
-	"9":     9,
-}
-
 func Trebuchet(input ...string) int {
 	// short-circuit if empty
 	if len(input) == 0 {
