57eaf7e4e9dcccf52d14cee214babc262f6e609a
zalgonoise <mariozalgo@gmail.com>
2023-12-01 14:43:59 +0100

    advent/day01/part-01: adding solution and tests

---
+++ day01/part-01/trebuchet.go
@@ -0,0 +1,47 @@
+package trebuchet
+
+import (
+	"strings"
+)
+
+func Trebuchet(input ...string) int {
+	// short-circuit if empty
+	if len(input) == 0 {
+		return 0
+	}
+
+	var n int
+
+	for i := range input {
+		// split by newlines if any
+		split := strings.Split(input[i], "\n")
+
+		for idx := range split {
+			n += trebuchet(split[idx])
+		}
+	}
+
+	return n
+}
+
+func trebuchet(input string) int {
+	var n int
+
+	for i := range input {
+		if input[i] >= '0' && input[i] <= '9' {
+			n = int(input[i]-'0') * 10
+
+			break
+		}
+	}
+
+	for i := len(input) - 1; i >= 0; i-- {
+		if input[i] >= '0' && input[i] <= '9' {
+			n += int(input[i] - '0')
+
+			break
+		}
+	}
+
+	return n
+}
---
+++ day01/part-01/trebuchet_test.go
@@ -0,0 +1,50 @@
+package trebuchet
+
+import (
+	"testing"
+
+	"github.com/zalgonoise/x/advent-2023/01/day01"
+)
+
+func TestTrebuchet(t *testing.T) {
+	for _, testcase := range []struct {
+		name  string
+		input []string
+		wants int
+	}{
+		{
+			name:  "Challenge",
+			input: []string{day01.Input},
+			wants: 55488,
+		},
+		{
+			name: "Example/AsString",
+			input: []string{`1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet`},
+			wants: 142,
+		},
+		{
+			name:  "Example/AsStringSlice",
+			input: []string{"1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"},
+			wants: 142,
+		},
+	} {
+		t.Run(testcase.name, func(t *testing.T) {
+			res := Trebuchet(testcase.input...)
+
+			isEqual(t, testcase.wants, res)
+		})
+	}
+}
+
+func isEqual[T comparable](t *testing.T, wants, got T) {
+	if got != wants {
+		t.Errorf("output mismatch error: wanted %v ; got %v", wants, got)
+
+		return
+	}
+
+	t.Logf("output matched expected value: %v", wants)
+}
