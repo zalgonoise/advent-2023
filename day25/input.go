508cfd42d0e309ec33a902cf1b19cdf31abfa740
zalgonoise <mariozalgo@gmail.com>
2023-12-25 13:20:10 +0100

    advent/day25: embedded generated input for day 25

---
+++ day25/input.go
@@ -0,0 +1,22 @@
+package day25
+
+import _ "embed"
+
+//go:embed input/input.txt
+var Input string
+
+var TestInput = `
+jqt: rhn xhk nvd
+rsh: frs pzl lsr
+xhk: hfx
+cmg: qnr nvd lhk bvb
+rhn: xhk bvb hfx
+bvb: xhk hfx
+pzl: lsr hfx nvd
+qnr: nvd
+ntq: jqt hfx bvb xhk
+nvd: lhk
+lsr: lhk
+rzs: qnr cmg lsr rsh
+frs: qnr lhk lsr
+`
